0b012316e8d59a11c934cc9ae21fb9043c9e363f
Junseok Oh <kotohana5706@edcan.kr>
2017-06-12 19:55:43 +0900

    Add activities, layout resource file

--- app/src/main/java/moe/kotohana/randomfood/MainActivity.java
+++ app/src/main/java/moe/kotohana/randomfood/MainActivity.java
@@ -1,7 +1,13 @@
 package moe.kotohana.randomfood;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import moe.kotohana.randomfood.R;
 
@@ -11,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((ViewPager) findViewById(R.id.viewPager)).setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                // TODO Start SettingsActivity
+//                startActivity(new Intent(getApplicationContext(), ));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/moe/kotohana/randomfood/NearFoodMapActivity.java
@@ -0,0 +1,13 @@
+package moe.kotohana.randomfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NearFoodMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_near_food_map);
+    }
+}
