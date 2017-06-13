e28b6b4396f773f150b664514f1bb73d3101de28
Junseok Oh <kotohana5706@edcan.kr>
2017-06-13 10:35:23 +0900

    Implement Place Model

---
+++ app/src/main/java/moe/kotohana/randomfood/models/Place.java
@@ -0,0 +1,17 @@
+package moe.kotohana.randomfood.models;
+
+/**
+ * Created by Junseok Oh on 2017-06-13.
+ */
+
+public class Place {
+    private String name;
+
+    public Place(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
