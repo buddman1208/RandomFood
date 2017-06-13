78239bcff02c4a6a18e1a230920dff4af08ef4d4
Junseok Oh <kotohana5706@edcan.kr>
2017-06-14 00:34:00 +0900

    Build UserSettingsActivity layout * Add PieCharty

--- app/src/main/java/moe/kotohana/randomfood/UserSettingsActivity.java
+++ app/src/main/java/moe/kotohana/randomfood/UserSettingsActivity.java
@@ -1,13 +1,48 @@
 package moe.kotohana.randomfood;
 
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.intrusoft.scatter.ChartData;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import moe.kotohana.randomfood.databinding.ActivityUserSettingsBinding;
 
 public class UserSettingsActivity extends AppCompatActivity {
 
+    ActivityUserSettingsBinding binding;
+    ArrayList<ChartData> arrayList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_settings);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_settings);
+        setData();
+        binding.developerInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity();
+            }
+        });
+        binding.showHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity();
+            }
+        });
+    }
+
+    private void setData() {
+        Collections.addAll(arrayList,
+                new ChartData("한식", 20, Color.WHITE, Color.BLUE),
+                new ChartData("한식", 40, Color.WHITE, Color.RED),
+                new ChartData("한식", 20, Color.WHITE, Color.GRAY),
+                new ChartData("한식", 20, Color.WHITE, Color.GREEN)
+        );
+        binding.chart.setChartData(arrayList);
     }
 }
