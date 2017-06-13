55cb0c534165b95f2453d0093cad55b44a9f83f9
Junseok Oh <kotohana5706@edcan.kr>
2017-06-13 09:47:55 +0900

    Start activity when cardview is clicked @MainActivity

--- app/src/main/java/moe/kotohana/randomfood/MainActivity.java
+++ app/src/main/java/moe/kotohana/randomfood/MainActivity.java
@@ -1,6 +1,7 @@
 package moe.kotohana.randomfood;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -12,19 +13,18 @@ import android.view.View;
 import android.widget.Toast;
 
 import moe.kotohana.randomfood.R;
+import moe.kotohana.randomfood.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        (findViewById(R.id.cardview)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
-            }
-        });
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setCardViewClick();
         findViewById(R.id.settings).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +33,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setCardViewClick() {
+        binding.hansikFood.setOnClickListener(new CardClickListener().setPosition(0));
+        binding.chineseFood.setOnClickListener(new CardClickListener().setPosition(1));
+        binding.boonsikFood.setOnClickListener(new CardClickListener().setPosition(2));
+        binding.chickenFood.setOnClickListener(new CardClickListener().setPosition(3));
+        binding.fastFood.setOnClickListener(new CardClickListener().setPosition(4));
+        binding.pizzaFood.setOnClickListener(new CardClickListener().setPosition(5));
+        binding.japaneseFood.setOnClickListener(new CardClickListener().setPosition(6));
+    }
+
+    private class CardClickListener implements View.OnClickListener {
+        int position;
+
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(getApplicationContext(), NearFoodActivity.class)
+                    .putExtra("foodType", position));
+        }
+
+        View.OnClickListener setPosition(int position) {
+            this.position = position;
+            return this;
+        }
+
+    }
 
 }
